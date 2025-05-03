d2b0937144cc709d6feafe77c75d8a11f7adf944
mojitap <ta714kadvance@gmail.com>
2025-05-04 00:31:33 +0900

    Update app.py

--- app.py
+++ app.py
@@ -288,6 +288,6 @@ def legal():
     
 if __name__ == '__main__':
     with app.app_context():
-        if not os.path.exists('users.db'):
-            db.create_all()
+        db.create_all()
     port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
