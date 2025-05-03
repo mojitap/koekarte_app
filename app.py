b979bfc85c68fe741e9ee2b5723975834683ed40
mojitap <ta714kadvance@gmail.com>
2025-05-03 13:32:38 +0900

    Update app.py

--- app.py
+++ app.py
@@ -259,4 +259,5 @@ if __name__ == '__main__':
     with app.app_context():
         if not os.path.exists('users.db'):
             db.create_all()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
