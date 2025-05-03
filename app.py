33233911abdcef9b501f5fd73681e7fbeaf1ca28
mojitap <ta714kadvance@gmail.com>
2025-05-03 22:14:32 +0900

    Update app.py

--- app.py
+++ app.py
@@ -33,6 +33,10 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
+with app.app_context():
+    db.create_all()
+
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
