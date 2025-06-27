6cdcdbddd7199b50f0e6e909973a7f11c4b39a9b
mojitap <ta714kadvance@gmail.com>
2025-06-27 20:42:44 +0900

    Update app_instance.py

--- app_instance.py
+++ app_instance.py
@@ -1,7 +1,8 @@
+# app_instance.py
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
-app = Flask(__name__)
+app = Flask(__name__)  # 定義だけ
 db = SQLAlchemy()
 login_manager = LoginManager()
