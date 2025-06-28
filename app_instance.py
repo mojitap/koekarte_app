63ee4911d605a3b1ea50d7fab7b6591559cae8df
mojitap <ta714kadvance@gmail.com>
2025-06-28 15:46:07 +0900

    Update app_instance.py

--- app_instance.py
+++ app_instance.py
@@ -1,6 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object('config.Config')  # DB URIなどを読み込む
-db = SQLAlchemy(app)  # ← 直接バインド方式に変更（推奨）
+
+db = SQLAlchemy(app)
+
+# ✅ ここを追加してください（login_managerの初期化）
+login_manager = LoginManager()
+login_manager.init_app(app)
