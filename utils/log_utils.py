c54678c88da09704779baae2ccbfa35b9c3ee21b
mojitap <ta714kadvance@gmail.com>
2025-06-30 00:33:29 +0900

    Update log_utils.py

--- utils/log_utils.py
+++ utils/log_utils.py
@@ -1,8 +1,13 @@
 # utils/log_utils.py
 
 from models import db, ActionLog
+from flask_login import current_user
 
-def add_action_log(user_id, action):
-    log = ActionLog(user_id=user_id, action=action)
+def add_action_log(admin_email, action, user_email=None):
+    log = ActionLog(
+        admin_email=admin_email,
+        user_email=user_email or admin_email,
+        action=action
+    )
     db.session.add(log)
     db.session.commit()
