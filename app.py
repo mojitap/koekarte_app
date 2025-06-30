6ab711ecd89376342eb789ceda439a40bd074d13
mojitap <ta714kadvance@gmail.com>
2025-06-30 14:26:39 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1010,7 +1010,7 @@ def create_checkout_session():
         checkout_session = stripe.checkout.Session.create(
             payment_method_types=['card'],
             line_items=[{
-                'price': os.getenv("STRIPE_PRICE_ID"),  # .env に設定済み
+                'price': os.getenv("STRIPE_TEST_PRICE_ID"),  # .env に設定済み
                 'quantity': 1,
             }],
             mode='subscription',
