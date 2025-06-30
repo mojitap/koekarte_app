d1daae8b9c54a702bbea4fa3844d5b2a7ade36f1
mojitap <ta714kadvance@gmail.com>
2025-06-30 10:35:50 +0900

    Update log_utils.py

--- utils/log_utils.py
+++ utils/log_utils.py
@@ -1,9 +1,13 @@
-# utils/log_utils.py
-
-from models import db, ActionLog
+from models import db, ActionLog, User
 from flask_login import current_user
 
-def add_action_log(admin_email, action, user_email=None):
+def add_action_log(admin_email, action, user_email=None, user_id=None):
+    # 安全対策：user_emailがない場合、user_idから引き直す
+    if not user_email and user_id:
+        user = User.query.get(user_id)
+        if user:
+            user_email = user.email
+
     log = ActionLog(
         admin_email=admin_email,
         user_email=user_email or admin_email,
