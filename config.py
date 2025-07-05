35269477b234c7190ca5e1493df549e4a35070a7
mojitap <ta714kadvance@gmail.com>
2025-07-05 16:16:32 +0900

    Update config.py

--- config.py
+++ config.py
@@ -5,3 +5,7 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or 'sqlite:///default.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get("SECRET_KEY") or 'dev-secret-key'
+    SESSION_COOKIE_SECURE = True
+    SESSION_COOKIE_SAMESITE = 'None'
+    SESSION_COOKIE_DOMAIN = '.koekarte.com'
+    SESSION_COOKIE_HTTPONLY = True  # セキュリティ観点
